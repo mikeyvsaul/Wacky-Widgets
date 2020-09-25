dea5184ce77f70265ec38e8dfd82f297bae46be9
Michael Saul <michaelsaul@MacBook-Pro.local>
2020-09-25 11:47:15 -0700

    Complete Step 3.1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Widget
+
 
 # Create your views here.
+def index(request):
+    widgets = Widget.objects.all()
+    return render(request, 'index.html', {'widgets': widgets})
